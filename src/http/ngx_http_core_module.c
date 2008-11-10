6be5bac511905e8ca53e51e68e63e1a2e81fbf09
Igor Sysoev <igor@sysoev.ru>
2008-11-10 16:31:22 +0000

    Changes with nginx 0.7.20                                        10 Nov 2008
        *) Changes in the ngx_http_gzip_filter_module.
    
        *) Feature: the ngx_http_limit_req_module.
    
        *) Bugfix: worker processes might exit on a SIGBUS signal on sparc and
           ppc platforms; the bug had appeared in 0.7.3.
           Thanks to Maxim Dounin.
    
        *) Bugfix: the "proxy_pass http://host/some:uri" directives did not
           work; the bug had appeared in 0.7.12.
    
        *) Bugfix: in HTTPS mode requests might fail with the "bad write retry"
           error.
    
        *) Bugfix: the ngx_http_secure_link_module did not work inside
           locations, whose names are less than 3 characters.
    
        *) Bugfix: $server_addr variable might have no value.

--- src/http/ngx_http_core_module.c
+++ src/http/ngx_http_core_module.c
@@ -1631,6 +1631,10 @@ ngx_http_server_addr(ngx_http_request_t *r, ngx_str_t *s)
         }
 
         r->in_addr = sin.sin_addr.s_addr;
+
+    } else {
+        sin.sin_family = c->sockaddr->sa_family;
+        sin.sin_addr.s_addr = r->in_addr;
     }
 
     if (s == NULL) {
@@ -3470,7 +3474,7 @@ ngx_http_core_directio(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     value = cf->args->elts;
 
     if (ngx_strcmp(value[1].data, "off") == 0) {
-        clcf->directio = NGX_MAX_OFF_T_VALUE;
+        clcf->directio = NGX_OPEN_FILE_DIRECTIO_OFF;
         return NGX_CONF_OK;
     }
 

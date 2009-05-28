e600ee24fa8186fb055ed77d52242bfaeb8abb3a
Valery Kholodkov <valery@cruiser.(none)>
2009-05-28 11:41:13 +0200

    Doing eval as a first step of rewrite phase turns out to be a better idea

--- ngx_http_eval_module.c
+++ ngx_http_eval_module.c
@@ -348,19 +348,12 @@ ngx_http_eval_init(ngx_conf_t *cf)
 {
     ngx_http_handler_pt        *h;
     ngx_http_core_main_conf_t  *cmcf;
-    ngx_uint_t                  i;
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
     h = ngx_array_push(&cmcf->phases[NGX_HTTP_REWRITE_PHASE].handlers);
     if (h == NULL) {
         return NGX_ERROR;
-    }
-
-    h = cmcf->phases[NGX_HTTP_REWRITE_PHASE].handlers.elts;
-
-    for(i = cmcf->phases[NGX_HTTP_REWRITE_PHASE].handlers.nelts - 1; i > 0; i--) {
-        h[i] = h[i - 1];
     }
 
     *h = ngx_http_eval_handler;

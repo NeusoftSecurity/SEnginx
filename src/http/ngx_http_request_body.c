49590f7810331a2b33e8a120017f3a01aeab2176
Igor Sysoev <igor@sysoev.ru>
2009-09-28 13:08:15 +0000

    Changes with nginx 0.8.17                                        28 Sep 2009
        *) Security: now "/../" are disabled in "Destination" request header
           line.
    
        *) Change: now $host variable value is always low case.
    
        *) Feature: the $ssl_session_id variable.
    
        *) Bugfix: socket leak; the bug had appeared in 0.8.11.

--- src/http/ngx_http_request_body.c
+++ src/http/ngx_http_request_body.c
@@ -477,8 +477,9 @@ ngx_http_discard_request_body(ngx_http_request_t *r)
         return NGX_HTTP_INTERNAL_SERVER_ERROR;
     }
 
-    r->count++;
-    (void) ngx_http_read_discarded_request_body(r);
+    if (ngx_http_read_discarded_request_body(r) != NGX_OK) {
+        r->count++;
+    }
 
     return NGX_OK;
 }
@@ -562,10 +563,14 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
     ngx_log_debug0(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
                    "http read discarded body");
 
-    do {
+    for ( ;; ) {
         if (r->headers_in.content_length_n == 0) {
             r->read_event_handler = ngx_http_block_reading;
             return NGX_OK;
+        }
+
+        if (!r->connection->read->ready) {
+            return NGX_AGAIN;
         }
 
         size = (r->headers_in.content_length_n > NGX_HTTP_DISCARD_BUFFER_SIZE) ?
@@ -588,10 +593,7 @@ ngx_http_read_discarded_request_body(ngx_http_request_t *r)
         }
 
         r->headers_in.content_length_n -= n;
-
-    } while (r->connection->read->ready);
-
-    return NGX_AGAIN;
+    }
 }
 
 

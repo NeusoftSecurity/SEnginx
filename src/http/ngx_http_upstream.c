c4841987988be7c9525e4d2992c894d8ad7e3d92
Igor Sysoev <igor@sysoev.ru>
2009-10-14 12:57:31 +0000

    Changes with nginx 0.8.20                                        14 Oct 2009
        *) Change: now default SSL ciphers are "HIGH:!ADH:!MD5".
    
        *) Bugfix: the ngx_http_autoindex_module did not show the trailing
           slash in links to a directory; the bug had appeared in 0.7.15.
    
        *) Bugfix: nginx did not close a log file set by the --error-log-path
           configuration option; the bug had appeared in 0.7.53.
    
        *) Bugfix: nginx did not treat a comma as separator in the
           "Cache-Control" backend response header line.
    
        *) Bugfix: nginx/Windows might not create temporary file, a cache file,
           or "proxy/fastcgi_store"d file if a worker has no enough access
           rights for top level directories.
    
        *) Bugfix: the "Set-Cookie" and "P3P" FastCGI response header lines
           were not hidden while caching if no "fastcgi_hide_header" directives
           were used with any parameters.
    
        *) Bugfix: nginx counted incorrectly disk cache size.

--- src/http/ngx_http_upstream.c
+++ src/http/ngx_http_upstream.c
@@ -2098,11 +2098,6 @@ ngx_http_upstream_send_response(ngx_http_request_t *r, ngx_http_upstream_t *u)
             r->cache->date = now;
             r->cache->body_start = (u_short) (u->buffer.pos - u->buffer.start);
 
-            if (r->headers_out.content_length_n != -1) {
-                r->cache->length = r->cache->body_start
-                                   + r->headers_out.content_length_n;
-            }
-
             ngx_http_file_cache_set_header(r, u->buffer.start);
 
         } else {
@@ -3040,7 +3035,7 @@ ngx_http_upstream_process_cache_control(ngx_http_request_t *r,
     n = 0;
 
     for (p += 8; p < last; p++) {
-        if (*p == ';' || *p == ' ') {
+        if (*p == ',' || *p == ';' || *p == ' ') {
             break;
         }
 

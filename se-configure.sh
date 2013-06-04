821c5c829cddc2fd3a2c6e9fa25558809f127a86
paulyang <y_y@neusoft.com>
2013-06-04 10:46:52 +0800

    change: delete some configurations in se-configure.sh
    Signed-off-by: paulyang <y_y@neusoft.com>

--- se-configure.sh
+++ se-configure.sh
@@ -3,16 +3,10 @@
 TRD_DIR=$PWD/3rd-party
 
 ./configure $* \
-    --with-mail \
-    --with-mail_ssl_module \
-    --with-ipv6 \
-    --with-http_ssl_module \
-    --with-debug \
     --add-module=${TRD_DIR}/ngx_http_neteye_security \
     --add-module=${TRD_DIR}/ngx_http_naxsi_neteye_helper \
     --add-module=${TRD_DIR}/naxsi/naxsi_src \
     --add-module=${TRD_DIR}/nginx-upstream-fair \
-    --add-module=${TRD_DIR}/nginx-eval-module \
     --add-module=${TRD_DIR}/headers-more-nginx-module \
     --add-module=${TRD_DIR}/ngx_http_substitutions_filter_module \
     --add-module=${TRD_DIR}/nginx_tcp_proxy_module \

6aa9fe4f5871d71b84a0de6be810f2a9b1685f5b
jason <liu_yt@neusoft.com>
2013-06-18 13:38:06 +0800

    change: use --without-modsecurity to not compile with modsecurity
    Signed-off-by: jason <liu_yt@neusoft.com>

--- se-configure.sh
+++ se-configure.sh
@@ -4,24 +4,24 @@ set -e
 
 TRD_DIR=$PWD/3rd-party
 MOD_SECURITY_DIR=${TRD_DIR}/ModSecurity
-MOD_SECURITY_CONFIG='--with-modsecurity'
-unset HAVE_MOD_SECURITY
+NO_MOD_SECURITY_CONFIG='--without-modsecurity'
+unset HAVE_NO_MOD_SECURITY
 for arg in $*
 do
-    if [ $arg = $MOD_SECURITY_CONFIG ]; then
-        HAVE_MOD_SECURITY='y'
+    if [ $arg = $NO_MOD_SECURITY_CONFIG ]; then
+        HAVE_NO_MOD_SECURITY='y'
         break;
     fi
 done
 
 if [ $# -ne 0 ]; then
-    NGX_ARGS=`echo $* | sed "s/$MOD_SECURITY_CONFIG//"`
+    NGX_ARGS=`echo $* | sed "s/$NO_MOD_SECURITY_CONFIG//"`
 else
     unset NGX_ARGS
 fi
 
-if [ ! -z $HAVE_MOD_SECURITY ]; then
-    echo "compile with modsecurity"
+if [ -z $HAVE_NO_MOD_SECURITY ]; then
+    echo "Compile with modsecurity, use $NO_MOD_SECURITY_CONFIG to cancel"
     cd $MOD_SECURITY_DIR
     ./configure --enable-standalone-module $NGX_ARGS
     make
@@ -56,7 +56,7 @@ get_line_num()
     done
 }
 
-if [ ! -z $HAVE_MOD_SECURITY ]; then
+if [ -z $HAVE_NO_MOD_SECURITY ]; then
     sed -i "/clean:/ a \\\tcd $MOD_SECURITY_DIR;make clean" Makefile
     sed -i "/build:/ a \\\tcd $MOD_SECURITY_DIR;\$(MAKE) -f Makefile" Makefile
     PREFIX=`grep ^#define objs/ngx_auto_config.h | grep NGX_PREFIX | awk '{print $3}' | sed 's/"//' | sed 's/"//'`
